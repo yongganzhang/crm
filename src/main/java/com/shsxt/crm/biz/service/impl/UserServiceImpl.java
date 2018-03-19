47e4f510c65ef439a8931739e17a333715eda994
yongganzhang <1285963544@qq.com>
2018-03-19 09:34:06 +0800

    完成配置SSM和测试基础环境

---
+++ src/main/java/com/shsxt/crm/biz/service/impl/UserServiceImpl.java
@@ -0,0 +1,19 @@
+package com.shsxt.crm.biz.service.impl;
+
+import com.shsxt.crm.biz.dao.UserDao;
+import com.shsxt.crm.biz.model.User;
+import com.shsxt.crm.biz.service.IUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl  implements IUserService{//提示  alt + enter
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public User get(int id) {
+        return userDao.get(id);
+    }
+}
